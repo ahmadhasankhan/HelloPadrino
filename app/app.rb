f3d84462ff9cf709db1742b071f7829ac5faf4fe
ahmadhasankhan <er.ahmad.hassan@gmail.com>
2015-01-12 12:37:06 +0530

    added admin panel

--- app/app.rb
+++ app/app.rb
@@ -5,6 +5,13 @@ module SampleApp
     register Padrino::Mailer
     register Padrino::Helpers
 
+    get "/" do
+      "Hello World!"
+    end
+
+    get :about, :map => '/about_us' do
+      render :haml, "%p This is a sample blog created to demonstrate how Padrino works!"
+    end
     enable :sessions
 
     ##
